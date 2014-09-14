e245e7763317403e5f0a00fad95516c65c7d4e4d
Tarush <tarushbali@gmail.com>
2014-09-13 17:24:29 -0700

    INIT

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,25 +1,59 @@
 package controllers
 
+import java.io._
+
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3Client
-import play.api.mvc.{Controller, Action}
-import awscala._, s3._
-import iam._
+import com.amazonaws.services.s3.model.S3Object
+import play.api.libs.json.Json
+import play.api.mvc.{Action, Controller}
 
 object Application extends Controller {
-  implicit val s3 = S3()
-  implicit val iam = IAM()
-  val AWS_ACCESS_KEY = "AKIAI7PGQWZSCORB66SQ"
-  val AWS_SECRET_KEY = "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"
-  val bucketName = "neelkanthbucket"          // specifying bucket name
+  val AWS_ACCESS_KEY = "AKIAJMABWBEQQ3WRFCYA"
+  val AWS_SECRET_KEY = "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"
+  val bucketName = "lockscreens22"          // specifying bucket name
 
   def index = Action {
+    Ok(views.html.index())
+  }
+
+  def latestVersion = Action {
+    Ok(Json.obj("status" ->"OK", "version" -> "1"))
+  }
+
+  def upload = Action(parse.multipartFormData) { request =>
+    request.body.file("picture").map { picture =>
+      import java.io.File
+      val filename = picture.filename
+      val contentType = picture.contentType
+      val file = new File("/tmp/picture")
+      picture.ref.moveTo(file, true)
+      val yourAWSCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+      val amazonS3Client = new AmazonS3Client(yourAWSCredentials)
+      amazonS3Client.putObject(bucketName, System.currentTimeMillis().toString, file)
+      Ok("File uploaded")
+    }.getOrElse {
+      Redirect(routes.Application.index).flashing(
+        "error" -> "Missing file"
+      )
+    }
+  }
+
+  def download(timestamp: Long) = Action {
     val yourAWSCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY)
     val amazonS3Client = new AmazonS3Client(yourAWSCredentials)
-    amazonS3Client.createBucket(bucketName)
+    val downloadedObject: S3Object = amazonS3Client.getObject(bucketName, timestamp.toString)
 
+    val reader = new BufferedInputStream(downloadedObject.getObjectContent)
+    val file: File = new File("/tmp/" + timestamp.toString)
+    val writer = new BufferedOutputStream(new FileOutputStream(file))
 
-    Ok(views.html.index("Your new application is ready."))
+    Iterator
+      .continually (reader.read)
+      .takeWhile (-1 !=)
+      .foreach (writer.write)
+
+    Ok.sendFile(file)
   }
 
 }
